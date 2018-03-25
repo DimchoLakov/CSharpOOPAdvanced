f612786a60c94fca1e85d7e77922655df50dfde8
whywherewhat <mdsektor@gmail.com>
2018-03-25 02:16:08 +0100

    Added 02. Generic Array Creator

---
+++ 02.Generics - Lab/02.GenericArrayCreator/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        string[] strings = ArrayCreator.Create(5, "some data");
+        int[] integers = ArrayCreator.Create(10, 33);
+
+        foreach (var s in strings)
+        {
+            Console.WriteLine(s);
+        }
+
+        foreach (var integer in integers)
+        {
+            Console.WriteLine(integer);
+        }
+    }
+}
