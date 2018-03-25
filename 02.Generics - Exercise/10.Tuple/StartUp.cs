5e9b54d02728f1481237cd3d98332be64d9b9ba0
whywherewhat <mdsektor@gmail.com>
2018-03-26 00:36:36 +0100

    Added 10.Tuple

---
+++ 02.Generics - Exercise/10.Tuple/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        string[] tokens = ReadInput();
+        tokens[0] = $"{tokens[0]} {tokens[1]}";
+        var firstTuple = new Tuple<string, string>(tokens[0], tokens[2]);
+        PrintTuple(firstTuple);
+
+        tokens = ReadInput();
+        var secondTuple = new Tuple<string, int>(tokens[0], int.Parse(tokens[1]));
+        PrintTuple(secondTuple);
+
+        tokens = ReadInput();
+        var thirdTuple = new Tuple<int, double>(int.Parse(tokens[0]), double.Parse(tokens[1]));
+        PrintTuple(thirdTuple);
+
+    }
+
+    private static void PrintTuple<T1, T2>(Tuple<T1, T2> tuple)
+    {
+        Console.WriteLine($"{tuple.FirstItem} -> {tuple.SecondItem}");
+    }
+
+    private static string[] ReadInput()
+    {
+        return Console.ReadLine().Split();
+    }
+}
---
+++ 02.Generics - Exercise/10.Tuple/Tuple.cs
@@ -0,0 +1,16 @@
+﻿public class Tuple<T1, T2>
+{
+    public T1 FirstItem { get; set; }
+    public T2 SecondItem { get; set; }
+    
+    public Tuple()
+    {
+
+    }
+
+    public Tuple(T1 firstItem, T2 secondItem)
+    {
+        this.FirstItem = firstItem;
+        this.SecondItem = secondItem;
+    }
+}
