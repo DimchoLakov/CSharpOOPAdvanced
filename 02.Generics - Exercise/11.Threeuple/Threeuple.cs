7e8e352c3a01ffeb2f8df165d81c7b0d251997cf
whywherewhat <mdsektor@gmail.com>
2018-03-26 00:46:52 +0100

    Added 11. Threeuple

---
+++ 02.Generics - Exercise/11.Threeuple/Threeuple.cs
@@ -0,0 +1,20 @@
+﻿public class Threeuple<T1, T2, T3>
+{
+    public T1 FirstItem { get; set; }
+
+    public T2 SecondItem { get; set; }
+
+    public T3 ThirdItem { get; set; }
+    
+    public Threeuple()
+    {
+
+    }
+
+    public Threeuple(T1 firstItem, T2 secondItem, T3 thirdItem)
+    {
+        this.FirstItem = firstItem;
+        this.SecondItem = secondItem;
+        this.ThirdItem = thirdItem;
+    }
+}
