41ccf9cddc6a91b90fc28106d42c66f24f85aafb
whywherewhat <mdsektor@gmail.com>
2018-03-25 12:12:27 +0100

    Added 03. Scale

---
+++ 02.Generics - Lab/03.Scale/Scale.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public class Scale<T>
+    where T : IComparable<T>
+{
+    public T Left { get; private set; }
+
+    public T Right { get; private set; }
+
+    public Scale(T left, T right)
+    {
+        this.Left = left;
+        this.Right = right;
+    }
+
+    public T GetHeavier()
+    {
+        var compareToResult = this.Left.CompareTo(this.Right);
+
+        if (compareToResult < 0)
+        {
+            return this.Right;
+        }
+
+        if (compareToResult > 0)
+        {
+            return this.Left;
+        }
+
+        return default(T);
+    }
+}
---
+++ 02.Generics - Lab/03.Scale/StartUp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var scale = new Scale<char>('b', 'c');
+
+        Console.WriteLine(scale.GetHeavier());
+    }
+}
