1d358a9af6d38422f9b145a167b4ea4efe956526
whywherewhat <mdsektor@gmail.com>
2018-03-26 00:05:40 +0100

    Added 09. Custom ListIterator

---
+++ 02.Generics - Exercise/09.CustomListIterator/Sorter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Sorter
+{
+    public static void Sort<T>(CustomList<T> myList) where T : IComparable<T>
+    {
+        for (int i = 1; i <= myList.Count - 1; ++i)
+
+            for (int j = 0; j < myList.Count - i; ++j)
+
+                if (myList[j].CompareTo(myList[j + 1]) > 0)
+                {
+                    var temp = myList[j];
+                    myList[j] = myList[j + 1];
+                    myList[j + 1] = temp;
+                }
+    }
+}
---
+++ 02.Generics - Exercise/09.CustomListIterator/StartUp.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        CustomList<string> myCustomList = new CustomList<string>();
+
+        string input;
+
+        while ((input = Console.ReadLine()) != "END")
+        {
+            string[] tokens = input.Split();
+
+            string command = tokens[0];
+
+            string result = string.Empty;
+
+            switch (command)
+            {
+                case "Add":
+                    myCustomList.Add(tokens[1]);
+                    break;
+                case "Remove":
+                    myCustomList.Remove(int.Parse(tokens[1]));
+                    break;
+                case "Contains":
+                    result = myCustomList.Contains(tokens[1]).ToString();
+                    break;
+                case "Swap":
+                    myCustomList.Swap(int.Parse(tokens[1]), int.Parse(tokens[2]));
+                    break;
+                case "Greater":
+                    result = myCustomList.CountGreaterThan(tokens[1]).ToString();
+                    break;
+                case "Max":
+                    result = myCustomList.Max().ToString();
+                    break;
+                case "Min":
+                    result = myCustomList.Min().ToString();
+                    break;
+                case "Print":
+                    PrintElements(myCustomList);
+                    break;
+                case "Sort":
+                    //myCustomList.Sort();
+                    Sorter.Sort(myCustomList);
+                    break;
+            }
+
+            if (result != string.Empty)
+            {
+                Console.WriteLine(result);
+            }
+        }
+        
+    }
+
+    private static void PrintElements(CustomList<string> myCustomList)
+    {
+        foreach (string element in myCustomList)
+        {
+            Console.WriteLine(element);
+        }
+    }
+}
