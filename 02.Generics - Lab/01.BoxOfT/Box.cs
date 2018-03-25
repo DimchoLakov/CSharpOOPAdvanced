089f1c30edc51a5cc5475314298d7ca7d36d8d62
whywherewhat <mdsektor@gmail.com>
2018-03-25 02:02:59 +0100

    Added 02. Generics - Lab, 01. Box Of T

---
+++ 02.Generics - Lab/01.BoxOfT/Box.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+public class Box<T> : IEnumerable<T>
+{
+    private readonly List<T> items;
+
+    public Box()
+    {
+        this.items = new List<T>();
+    }
+
+    public IReadOnlyCollection<T> Items => this.items.AsReadOnly();
+
+    public void Add(T element)
+    {
+        this.items.Add(element);
+    }
+
+    public T Remove()
+    {
+        T element = this.items[this.items.Count - 1];
+        this.items.RemoveAt(this.items.Count - 1);
+        return element;
+    }
+
+    public int Count => this.items.Count;
+    public IEnumerator<T> GetEnumerator()
+    {
+        return this.items.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.items.GetEnumerator();
+    }
+}
---
+++ 02.Generics - Lab/01.BoxOfT/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class StartUp
+{
+    public static void Main()
+    {
+        Box<int> box = new Box<int> {1, 2, 3};
+        Console.WriteLine(box.Remove());
+        box.Add(4);
+        box.Add(5);
+        Console.WriteLine(box.Remove());
+        Console.WriteLine(box.Count);
+    }
+}
