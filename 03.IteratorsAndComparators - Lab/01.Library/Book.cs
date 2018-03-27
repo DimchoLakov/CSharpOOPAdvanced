61ef1bd4ccd3e2867aa61b9fe5eec6f5ec51f5d8
whywherewhat <mdsektor@gmail.com>
2018-03-27 21:44:11 +0100

    Added 03. Iterators And Comparators - Lab, 01. Library

---
+++ 03.IteratorsAndComparators - Lab/01.Library/Book.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+public class Book
+{
+    public Book(string title, int year, params string[] authors)
+    {
+        this.Title = title;
+        this.Year = year;
+        this.Authos = authors;
+    }
+
+    public string Title { get; set; }
+
+    public int Year { get; set; }
+
+    public ICollection<string> Authos { get; set; }
+}
---
+++ 03.IteratorsAndComparators - Lab/01.Library/Library.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Library : IEnumerable<Book>
+{
+    public Library(params Book[] books)
+    {
+        this.Books = books;
+    }
+
+    public ICollection<Book> Books { get; set; }
+
+    public IEnumerator<Book> GetEnumerator()
+    {
+        return this.Books.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
