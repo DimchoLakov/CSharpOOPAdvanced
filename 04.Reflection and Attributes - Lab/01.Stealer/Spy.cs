c9f00a55e4d3c6444c55df651c896c851f9079b9
whywherewhat <mdsektor@gmail.com>
2018-04-01 01:18:09 +0100

    Added 04. Reflection and Attributes, 01. Stealer

---
+++ 04.Reflection and Attributes - Lab/01.Stealer/Spy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+public class Spy
+{
+    public string StealFieldInfo(string nameOfClass, params string[] fields)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        Type typeOfClass = Type.GetType(nameOfClass);
+
+        sb.AppendLine($"Class under investigation: {nameOfClass}");
+
+        object hackerInstance = Activator.CreateInstance(typeOfClass, new object[] { });
+        //var hackerInstance = Activator.CreateInstance<Hacker>();
+
+        var fieldsArray = typeOfClass.GetFields(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+        foreach (FieldInfo fieldInfo in fieldsArray.Where(f => fields.Contains(f.Name)))
+        {
+            sb.AppendLine($"{fieldInfo.Name} = {fieldInfo.GetValue(hackerInstance)}");
+        }
+
+        return sb.ToString().Trim();
+    }
+}
---
+++ 04.Reflection and Attributes - Lab/01.Stealer/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        Spy spy = new Spy();
+        string result = spy.StealFieldInfo("Hacker", "username", "password");
+        Console.WriteLine(result);
+    }
+}
