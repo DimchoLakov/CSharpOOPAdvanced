7f340e23ba108273392f056eea5b4ac14ba730e5
whywherewhat <mdsektor@gmail.com>
2018-04-01 05:06:28 +0100

    Added 06. Tracker

---
+++ 04.Reflection and Attributes - Lab/06.Tracker/StartUp.cs
@@ -0,0 +1,10 @@
+﻿[SoftUni("Dimcho")]
+public class StartUp
+{
+    [SoftUni("Lakov")]
+    public static void Main()
+    {
+        Tracker tracker = new Tracker();
+        tracker.PrintMethodsByAuthor();
+    }
+}
---
+++ 04.Reflection and Attributes - Lab/06.Tracker/Tracker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+public class Tracker
+{
+    public void PrintMethodsByAuthor()
+    {
+        Type startupType = typeof(StartUp);
+
+        MethodInfo[] methods = startupType
+            .GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+
+        foreach (MethodInfo methodInfo in methods)
+        {
+            if (methodInfo.CustomAttributes.Any(n => n.AttributeType == typeof(SoftUniAttribute)))
+            {
+                object[] attributes = methodInfo.GetCustomAttributes(false);
+
+                foreach (SoftUniAttribute attribute in attributes)
+                {
+                    Console.WriteLine($"{methodInfo.Name} is written by {attribute.Name}");
+                }
+            }
+        }
+    }
+}
