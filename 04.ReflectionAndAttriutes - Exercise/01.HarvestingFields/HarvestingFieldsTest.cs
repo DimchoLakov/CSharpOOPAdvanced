fedd114bf9628126fefb867dd8f6c2e22f6fc5cd
whywherewhat <mdsektor@gmail.com>
2018-04-01 19:39:45 +0100

    Added 01. Harvesting Fields, 02. Black Box Integer

---
+++ 04.ReflectionAndAttriutes - Exercise/01.HarvestingFields/HarvestingFieldsTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+public class HarvestingFieldsTest
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+
+        while (input != "HARVEST")
+        {
+            string result = GetFieldsInfo(input);
+
+            Console.WriteLine(result);
+
+            input = Console.ReadLine();
+        }
+    }
+
+    private static string GetFieldsInfo(string input)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        Type typeOfHarvestingFields = Type.GetType(nameof(HarvestingFields));
+
+        var instance = (HarvestingFields)Activator.CreateInstance(typeof(HarvestingFields));
+
+        switch (input)
+        {
+            case "private":
+                FieldInfo[] privateFieldsInfo = typeOfHarvestingFields
+                    .GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+
+                foreach (FieldInfo fieldInfo in privateFieldsInfo.Where(f => f.IsPrivate))
+                {
+                    sb.AppendLine($"private {fieldInfo.FieldType.Name} {fieldInfo.Name}");
+                }
+
+                break;
+            case "protected":
+                FieldInfo[] protectedFieldsInfo = typeOfHarvestingFields
+                    .GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+
+                foreach (FieldInfo fieldInfo in protectedFieldsInfo.Where(f => f.IsFamily))
+                {
+                    sb.AppendLine($"protected {fieldInfo.FieldType.Name} {fieldInfo.Name}");
+                }
+
+                break;
+            case "public":
+                FieldInfo[] publicFieldsInfo = typeOfHarvestingFields
+                    .GetFields(BindingFlags.Public | BindingFlags.Instance);
+
+                foreach (FieldInfo fieldInfo in publicFieldsInfo)
+                {
+                    sb.AppendLine($"public {fieldInfo.FieldType.Name} {fieldInfo.Name}");
+                }
+
+                break;
+            case "all":
+                FieldInfo[] allDeclaredFieldsInfo = typeOfHarvestingFields
+                    .GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.DeclaredOnly);
+
+                foreach (FieldInfo fieldInfo in allDeclaredFieldsInfo)
+                {
+                    string accessModfierToString = fieldInfo.IsPublic ? "public" : fieldInfo.IsPrivate ? "private" : fieldInfo.IsFamily ? "protected" : "internal";
+                    sb.AppendLine($"{accessModfierToString} {fieldInfo.FieldType.Name} {fieldInfo.Name}");
+                }
+
+                break;
+        }
+
+        return sb.ToString().Trim();
+    }
+}
---
+++ 04.ReflectionAndAttriutes - Exercise/02.BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+public class BlackBoxIntegerTests
+{
+    public static void Main()
+    {
+        var blackBoxInstance = Activator.CreateInstance(typeof(BlackBoxInteger), true);
+
+        var typeOfBlackBox = Type.GetType(nameof(BlackBoxInteger));
+
+        MethodInfo[] methods = typeOfBlackBox.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+        string input = Console.ReadLine();
+
+        while (input != "END")
+        {
+            string[] tokens = input.Split('_');
+            string methodName = tokens[0];
+            int val = int.Parse(tokens[1]);
+
+            var method = methods.Where(m => m.Name == methodName).First();
+            method.Invoke(blackBoxInstance, new object[] { val });
+
+            FieldInfo field = typeOfBlackBox.GetField("innerValue", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            var value = field?.GetValue(blackBoxInstance);
+
+            Console.WriteLine(value);
+
+            input = Console.ReadLine();
+        }
+    }
+}
