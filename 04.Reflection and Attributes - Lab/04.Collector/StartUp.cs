b79b228c705ed9da356a811cdec72a3f932a27e9
whywherewhat <mdsektor@gmail.com>
2018-04-01 04:04:49 +0100

    Refactored 04. Collector

---
+++ 04.Reflection and Attributes - Lab/04.Collector/StartUp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        Spy spy = new Spy();
+        string result = spy.CollectGettersAndSetters(nameof(Hacker));
+        Console.WriteLine(result);
+    }
+}
